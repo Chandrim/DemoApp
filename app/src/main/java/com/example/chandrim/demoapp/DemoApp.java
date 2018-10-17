4414d5abff614b944b01e61cc8b66b0dbc64a939
chandrim <chandrim.gayen@gmail.com>
2018-10-17 09:49:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chandrim/demoapp/DemoApp.java
@@ -0,0 +1,44 @@
+package com.example.chandrim.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.util.ArrayList;
+
+public class DemoApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_app);
+
+        ListView listView= (ListView) findViewById(R.id.listView);
+
+        ArrayList<String> family= new ArrayList<String>();
+        family.add("Radha");
+        family.add("Parul");
+        family.add("Seha");
+
+        ArrayAdapter<String> arrayAdapter= new ArrayAdapter<String>(this,android.R.layout.simple_gallery_item, family);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("hello");
+            }
+        });
+
+        };
+
+
+    }
+
